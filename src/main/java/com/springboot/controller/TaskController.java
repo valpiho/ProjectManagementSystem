99b82f377d14a37f4b52d3a6483443f1f688dd0c
susoja <susanna.oja@hotmail.com>
2021-02-21 11:26:52 +0200

    Added schema.sql and refactored data.sql

--- src/main/java/com/springboot/controller/TaskController.java
+++ src/main/java/com/springboot/controller/TaskController.java
@@ -1,12 +1,7 @@
 package com.springboot.controller;
 
-import com.springboot.entity.Task;
-import com.springboot.service.ProjectService;
 import com.springboot.service.TaskService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
